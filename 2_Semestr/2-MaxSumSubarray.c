68d4050636b99bbc7fce296160fa34d735e2d7c1
Cristina-F <k.fefelova@g.nsu.ru>
2017-02-17 19:42:24 +0800

    Create 2-MaxSumSubarray.c

---
+++ 2_Semestr/2-MaxSumSubarray.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define SIZE_ARRAY 10
+void search_max_sum( int* array ) {
+	int max_sum = array[0];
+	int sum = 0;
+	int left = 0; 
+	int right = 0;
+	for ( int i = 0; i < SIZE_ARRAY; i++ ) {
+		sum = sum + array[i];
+		if ( sum > max_sum ) {
+			max_sum = sum;
+			right = i;
+		}
+		if ( sum < 0 ) {
+			sum = 0;
+			left = i + 1;
+		}
+	}
+	printf("left = %d; right = %d; max_sum = %d;\n", left, right, max_sum );
+}
+
+int main ( ) {
+	int array[SIZE_ARRAY] = { 1, -3, 5, -1, 6, 7, -9, 3, 4, 5 };
+	search_max_sum( array );
+	return 0;
+}
