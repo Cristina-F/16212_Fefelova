123f4a1581253cdb04408f52cb8e37945d0f911d
Cristina-F <k.fefelova@g.nsu.ru>
2017-04-13 22:51:50 +0700

    Update hashTable.c

--- 1_Semestr/lab9-hash_table/hashTable.c
+++ 1_Semestr/lab9-hash_table/hashTable.c
@@ -1,18 +1,46 @@
+#include "header.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "header.h"
 
-#define STRESS 2 // Максимальная длина цепочки
+#define SIZE 5// Начальный размер хеш-таблицы
 #define MAX_LENGTH 30 // Максимальная длинна имени
+#define MULT 3
 
-int SIZE_TAB[] = { 3, 17, 19, 37, 101, 211, 409, 701, 997, 2003, 4451, 7177, 9973  };
+// Проверка длинны имени
+int check_name( char* name ) {
+	for( int i = 0; '\0' != name[i]; i++ ) {
+		if ( i > MAX_LENGTH ) {
+			return -1;
+		}
+	}
+	return 0;
+}
+
+
+char* read_name ( char* name ) {
+	int num = 0;
+	while (1) {
+		num = scanf( "%s", name );
+		if ( ( 1 != num ) ) {
+			while ( '\n' != getchar( ) );
+		}
+		else if ( -1 == check_name( name ) ){
+			while ( '\n' != getchar( ) );
+		}
+		else {
+			break;
+		}
+		printf( "\n" );
+	}
+	return name;
+}
 
 //Создание  хеш-таблицы
-struct hash_table* create_table ( int size_table, int rehash ) {
+struct hash_table* create_table ( int size_table ) {
 	struct hash_table* hash_table = ( struct hash_table* )calloc( 1, sizeof( struct hash_table ) );
 	hash_table -> size = size_table;
-	hash_table -> rehash = rehash;
 	hash_table -> cell = ( struct table_cell**)calloc( hash_table -> size, sizeof( struct table_cell ) );
 	for ( int i = 0; i < hash_table -> size; i++ ) {
 		hash_table -> cell[i] = NULL;
@@ -21,10 +49,12 @@ struct hash_table* create_table ( int size_table, int rehash ) {
 }
 
 static int hash_function ( struct hash_table* hash_table, char* name ) {
-	int key = 0;
+	int long long  key = 0;
 	int divider = hash_table -> size;
+	int mult = 1;
 	for ( int i = 0; '\0' != name[i]; i++ ) {
-		key = key + ( int )name[i];
+		key = key + ( int )name[i] * mult;
+		mult *= ( MULT );
 	}
 	return ( key % divider );
 }
@@ -32,7 +62,7 @@ static int hash_function ( struct hash_table* hash_table, char* name ) {
 // Добавляет элемент в ячейку хеш-таблицы
 static void fill_cell( struct hash_table* hash_table, int index, char* name, int height, int weight, int name_length ) {
 	hash_table -> cell[index] = ( struct table_cell* )calloc(1, sizeof( struct table_cell ) );
-	hash_table -> cell[index] -> name = ( char* )calloc( name_length, sizeof( char ) );
+	hash_table -> cell[index] -> name = ( char* )calloc( name_length + 1, sizeof( char ) );
 	for ( int i = 0; i < name_length; i++ ) {
 		hash_table -> cell[index] -> name[i] = name[i];
 	}
@@ -43,7 +73,7 @@ static void fill_cell( struct hash_table* hash_table, int index, char* name, int
 //Добавляет элемент в цепочку
 static void fill_chain( struct hash_table* hash_table, struct table_cell* tail, int index, char* name, int height, int weight, int name_length ) {
 	tail -> next = ( struct table_cell* )calloc( 1, sizeof( struct table_cell ) );
-	tail -> next -> name = ( char* )calloc( name_length, sizeof( char ) );
+	tail -> next -> name = ( char* )calloc( name_length + 1, sizeof( char ) );
 	for ( int i = 0; i < name_length; i++ ) {
 		tail -> next -> name[i] = name[i];
 	}
@@ -51,68 +81,68 @@ static void fill_chain( struct hash_table* hash_table, struct table_cell* tail, 
 	tail -> next -> weight = weight;
 }
 
-//Перехеширование таблицы
-static struct hash_table* rehashing( struct hash_table* hash_table, int* rehash ) {
-	*rehash = 0;
-	hash_table -> rehash = ( hash_table -> rehash ) + 1;
-	int new_size_table = SIZE_TAB[hash_table -> rehash];
-	struct hash_table* new_hash_table = create_table( new_size_table, hash_table -> rehash );
-	for ( int i = 0; i < hash_table -> size; i++ ) {
-		struct table_cell* tmp = hash_table -> cell[i];
-		if ( NULL != tmp ) {
-			while ( NULL != tmp ){
-				int new_index = hash_function( new_hash_table, tmp -> name  );
-				if ( NULL == new_hash_table -> cell[new_index] ) {
-					fill_cell( new_hash_table, new_index, tmp -> name, tmp -> height, tmp -> weight, strlen( tmp -> name));
-				}
-				else {	
-					int count  = 1;
-					struct table_cell* tail = new_hash_table -> cell[new_index];
-					while ( NULL != tail -> next ) {
-						count++;
-						if ( count >= STRESS ) {
-							*rehash = 1;
-						}
-						tail = tail-> next;
-					}
-					fill_chain( new_hash_table, tail, new_index, tmp -> name, tmp -> height,tmp -> weight, strlen( tmp -> name) );
-				}
-				tmp = tmp -> next;
-			}
+// Освобождение память хеш-таблицы
+void free_table ( struct hash_table* hash_table ) {
+	struct table_cell** elem = hash_table -> cell;
+	for ( int i = 0; i < hash_table -> size; i++ ){
+		while ( NULL != hash_table -> cell[i] ) {
+			struct table_cell* tmp = hash_table -> cell[i];
+			hash_table -> cell[i] = hash_table -> cell[i] -> next;
+			free( tmp -> name);
+			free( tmp );
 		}
+		free( hash_table -> cell[i] );
 	}
+	free( elem );
 	free( hash_table );
-	return new_hash_table;
 }
 
 // Запись данных в ячейку хеш-таблицы
-static struct hash_table* complete_table ( struct hash_table* hash_table, char* name, int height, int weight, int name_length, int* rehash ) {
+static struct hash_table* complete_table ( struct hash_table* hash_table, char* name, int height, int weight, int name_length ) {
 	int index = hash_function( hash_table, name );
+	printf("index = %d\n", index );
 	//Если ячейка пустая, то записываем в нее
 	if ( NULL == hash_table -> cell[index] ) {
 		fill_cell( hash_table, index, name, height, weight, name_length );
 	}
 	//Если ячейка занята, то записываем в конец цепочки
 	else {
-		int count  = 1;
 		struct table_cell* tail = hash_table -> cell[index];
 		while ( NULL != tail -> next ) {
-			count++;
-			if ( count >= STRESS ) {
-				*rehash = 1;
-			}
 			tail = tail -> next;
 		}
-		fill_chain( hash_table, tail, index, name, height, weight, name_length);
+		fill_chain( hash_table, tail, index, name, height, weight, name_length );
 	}
 	return hash_table;
 }
 
+//Перехеширование таблицы
+static struct hash_table* rehashing( struct hash_table* hash_table, char* name, int height, int weight ) {
+	int new_size_table = ( hash_table -> size ) * 10;
+	printf("new_size_table = %d; \n",new_size_table );
+	struct hash_table* new_hash_table = create_table( new_size_table );
+	for ( int i = 0; i < hash_table -> size; i++ ) {
+		struct table_cell* tmp = hash_table -> cell[i];
+		if ( NULL != tmp ) {
+			while ( NULL != tmp ){
+				new_hash_table = complete_table( new_hash_table, tmp -> name, tmp -> height, tmp -> weight, strlen( tmp -> name ) );
+				tmp = tmp -> next;
+			}
+		}
+
+	}
+	new_hash_table = complete_table( new_hash_table, name, height, weight, strlen( name ) );
+	free_table( hash_table );
+	return new_hash_table;
+}
+
 // Считываем данные из файла и записываем в хеш-таблицу
 struct hash_table* write_in_table( FILE* in_file, struct hash_table* hash_table ) {
-	int rehash = 0;
+	int count_elem = 0;
+	int i = 1;
 	while ( !feof( in_file ) ) {
 		int name_length = 0;
+		count_elem++;
 		while ( ' ' != fgetc( in_file ) ) {
 			if ( feof( in_file ) ) {
 				return hash_table;
@@ -134,56 +164,20 @@ struct hash_table* write_in_table( FILE* in_file, struct hash_table* hash_table 
 		fscanf( in_file, "%d", &height );
 		fscanf( in_file, "%d", &weight );
 		fgetc( in_file );
-		hash_table = complete_table( hash_table, name, height, weight, name_length , &rehash );
+		int stress = ( 3 *( hash_table -> size) ) / 4 ;
+		if ( count_elem > stress ) {
+			hash_table = rehashing( hash_table, name, height, weight );
+		}
+		else {
+			hash_table = complete_table( hash_table, name, height, weight, name_length );
+		}
 		free( name );
-	}
-	while ( 1 == rehash ){
-		hash_table = rehashing( hash_table, &rehash );
 	}
 	return hash_table;
 }
 
-// Проверка длинны имени
-int check_name( char* name ) {
-	for( int i = 0; '\0' != name[i]; i++ ) {
-		if ( i > MAX_LENGTH ) {
-			return -1;
-		}
-	}
-	return 0;
-}
-
-// Поиск в хеш-таблице
-struct table_cell* table_search ( struct hash_table* hash_table, char* name) {
-	int index = hash_function( hash_table, name );
-
-	if ( NULL == hash_table -> cell[index] ) {
-		return NULL;
-	}
-	else {
-		if ( NULL == hash_table -> cell[index] -> next ) {
-			if( 0 == strcmp( hash_table -> cell[index] -> name, name ) ) {
-				return hash_table -> cell[index];
-			}
-			else {
-				return NULL;
-			}
-		}
-		else {
-			struct table_cell* tmp  = hash_table -> cell[index];
-			while ( NULL != tmp ) {
-				if ( 0 == strcmp( tmp -> name, name ) ) {
-					return tmp;
-				}
-				tmp = tmp -> next;
-			}
-			return NULL;
-		}
-	}
-}
-
 // Печать результата поиска
-void print_search( struct table_cell* search ) {
+static void print_search( struct table_cell* search ) {
 	if ( NULL == search ) {
 		printf("Error: Data not found.\n" );
 	}
@@ -192,36 +186,36 @@ void print_search( struct table_cell* search ) {
 	}
 }
 
-// Печать хеш-таблицы
-void print_table ( struct hash_table* table ) {
-	printf("\n");
-	printf("size_table = %d;\n", table -> size );
-	printf("rehash = %d;\n", table -> rehash);
-	for ( int i = 0; i < table -> size; i++ ) {
-		int t = 0;
-		if ( NULL != table -> cell[i] ) {
-			struct table_cell* tmp = table -> cell[i];
-			while ( NULL != tmp ) {
-				printf( "%s ", tmp -> name );
-				printf( "%d ", tmp -> height );
-				printf( "%d ", tmp -> weight );
-				printf("index = %d;\n", i);
-				tmp = tmp -> next;
+// Поиск в хеш-таблице
+int table_search ( struct hash_table* hash_table, char* name ) {
+	struct table_cell* dont_search = NULL;
+	int index = hash_function( hash_table, name );
+	if ( NULL == hash_table -> cell[index] ) {
+		print_search( dont_search );
+		return -1;
+	}
+	else {
+		if ( NULL == hash_table -> cell[index] -> next ) {
+			if( 0 == strcmp( hash_table -> cell[index] -> name, name ) ) {
+				print_search( hash_table -> cell[index] );
+				return 0;
+			}
+			else {
+				print_search( dont_search );
+				return -1;
 			}
 		}
-	}
-	printf("\n");
-}
-
-// Освобождение память хеш-таблицы
-void free_table ( struct hash_table* hash_table ) {
-	for ( int i = 0; i < hash_table -> size; i++ ){
-		while ( NULL != hash_table -> cell[i] ) {
-			struct table_cell* tmp = hash_table -> cell[i];
-			hash_table -> cell[i] = hash_table -> cell[i] -> next;
-			free( tmp );
+		else {
+			struct table_cell* tmp  = hash_table -> cell[index];
+			while ( NULL != tmp ) {
+				if ( 0 == strcmp( tmp -> name, name )) {
+					print_search ( tmp );
+					return 0;
+				}
+					tmp = tmp -> next;
+			}
+			print_search( dont_search );
+			return -1;
 		}
-		free( hash_table -> cell[i] );
 	}
-	free( hash_table );
 }
