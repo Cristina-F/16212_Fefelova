5b2f1828a57d13e4a922a078474099f6e76619f6
Cristina-F <k.fefelova@g.nsu.ru>
2018-01-24 19:14:45 +0700

    GameOfLife

---
+++ 3_Semestr/build-GameOfLife-Desktop-Debug/moc_RenderArea.cpp
@@ -0,0 +1,154 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'RenderArea.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../GameOfLife/RenderArea.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'RenderArea.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_RenderArea_t {
+    QByteArrayData data[8];
+    char stringdata0[53];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_RenderArea_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_RenderArea_t qt_meta_stringdata_RenderArea = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "RenderArea"
+QT_MOC_LITERAL(1, 11, 9), // "pauseGame"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 11), // "changedCell"
+QT_MOC_LITERAL(4, 34, 1), // "x"
+QT_MOC_LITERAL(5, 36, 1), // "y"
+QT_MOC_LITERAL(6, 38, 6), // "status"
+QT_MOC_LITERAL(7, 45, 7) // "repaint"
+
+    },
+    "RenderArea\0pauseGame\0\0changedCell\0x\0"
+    "y\0status\0repaint"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_RenderArea[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       3,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   29,    2, 0x06 /* Public */,
+       3,    3,   30,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       7,    0,   37,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Bool,    4,    5,    6,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void RenderArea::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        RenderArea *_t = static_cast<RenderArea *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->pauseGame(); break;
+        case 1: _t->changedCell((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< bool(*)>(_a[3]))); break;
+        case 2: _t->repaint(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (RenderArea::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&RenderArea::pauseGame)) {
+                *result = 0;
+            }
+        }
+        {
+            typedef void (RenderArea::*_t)(int , int , bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&RenderArea::changedCell)) {
+                *result = 1;
+            }
+        }
+    }
+}
+
+const QMetaObject RenderArea::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_RenderArea.data,
+      qt_meta_data_RenderArea,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *RenderArea::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *RenderArea::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_RenderArea.stringdata0))
+        return static_cast<void*>(const_cast< RenderArea*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int RenderArea::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void RenderArea::pauseGame()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
+}
+
+// SIGNAL 1
+void RenderArea::changedCell(int _t1, int _t2, bool _t3)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)), const_cast<void*>(reinterpret_cast<const void*>(&_t3)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_END_MOC_NAMESPACE
